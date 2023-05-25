794dae7550df3f4a351014eb5df52da52d67b879
Hernan Pormachi Quichca <encora-hernan.pormachi@mibanco.com.pe>
2023-05-25 15:41:22 -0500

    WelcomeScreen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -10,6 +10,22 @@ class WelcomeScreen extends StatefulWidget {
 class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+        body: SafeArea(
+            child: Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 25,
+          horizontal: 35,
+        ),
+        child: Column(
+          children: [
+            Image.asset("assets/image1.png", height: 300),
+            const SizedBox(height: 20),
+            const Text("Let's get started"),
+          ],
+        ),
+      ),
+    )));
   }
 }
