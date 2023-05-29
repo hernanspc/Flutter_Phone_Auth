da78c4c7d4a50206d306b92e13508218387f789d
Hernan Pormachi Quichca <encora-hernan.pormachi@mibanco.com.pe>
2023-05-29 11:21:16 -0500

    agregar pantalla de verigicacion

--- lib/screens/otp_screen.dart
+++ lib/screens/otp_screen.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:phoneauth_firebase/utils/utils.dart';
+import 'package:phoneauth_firebase/widgets/custom_button.dart';
+import 'package:pinput/pinput.dart';
 
 class OtpScreen extends StatefulWidget {
   final String verificationId;
@@ -9,8 +14,116 @@ class OtpScreen extends StatefulWidget {
 }
 
 class _OtpScreenState extends State<OtpScreen> {
+  String? otpCode;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 30),
+            child: Column(
+              children: [
+                Align(
+                    alignment: Alignment.topLeft,
+                    child: GestureDetector(
+                      onTap: () => Navigator.of(context).pop(),
+                      child: const Icon(Icons.arrow_back),
+                    )),
+                Container(
+                  width: 200,
+                  height: 200,
+                  padding: const EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.purple.shade50,
+                  ),
+                  child: Image.asset(
+                    "assets/image2.png",
+                  ),
+                ),
+                //SEPARADOR
+                const SizedBox(height: 20),
+                //SEPARADOR
+                const Text(
+                  "Verificación",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Ingresa el numero de otp que te debe haber llegado",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                //SEPARADOR
+                const SizedBox(height: 20),
+                Pinput(
+                  length: 6,
+                  showCursor: true,
+                  defaultPinTheme: PinTheme(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(color: Colors.purple.shade200),
+                    ),
+                    textStyle: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  onSubmitted: (value) {
+                    setState(() {
+                      otpCode = value;
+                    });
+                  },
+                ),
+                const SizedBox(height: 25),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 50,
+                  child: CustomButton(
+                    text: "Verify",
+                    onPressed: () {
+                      if (otpCode != null) {
+                        verifyOtp(context, otpCode!);
+                      } else {
+                        showSnackBar(context, "Ingresa el código de 6 digitos");
+                      }
+                    },
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "No recibiste ningun codigo?",
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black38),
+                ),
+                const SizedBox(height: 15),
+                const Text(
+                  "Reenviar nuevo código",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.purple,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
+
+  void verifyOtp(BuildContext context, String userOtp) {}
 }
